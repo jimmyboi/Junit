10839217120502132856ee618777c469e43d7166
RahulPC <RahulPC@Rahul>
2017-11-12 23:21:36 +0530

    This is Junit

---
+++ src/JUnitTestExample/JUnitTesting.java
@@ -0,0 +1,10 @@
+package JUnitTestExample;
+
+public class JUnitTesting {
+
+	public String addStr(String string, String string2) {
+		// TODO Auto-generated method stub
+		return string+string2;
+	}
+
+}
---
+++ src/JUnitTestExample/testaddnumbers.java
@@ -0,0 +1,10 @@
+package JUnitTestExample;
+
+public class testaddnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
